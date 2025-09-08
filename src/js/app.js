9657967d7f8db67fc4cc041962a4e699b69ae615
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2025-09-08 15:43:24 +0300

    gh-pages update

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import '../css/style.css';
-import Field from './field';
-import GamePlay from './gameplay';
-
-const field = document.querySelector('.cells-container');
-const game = document.querySelector('.game');
-const fieldClass = new Field(field);
-const gamePlay = new GamePlay(game, fieldClass);
